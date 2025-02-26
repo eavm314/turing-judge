25cfd9bcae492cdbc9576c378d09737ecf8a6a22
Enrique Vicente <enriqueadhemar@gmail.com>
2025-02-26 18:33:52 -0400

    feat: dynamic canvas

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,9 +1,23 @@
-import { Canvas } from "@/components/editor/canvas"
+"use client"
+
 import { SideMenu } from "@/components/editor/side-menu"
+import { Loader } from "lucide-react";
+import dynamic from "next/dynamic";
+
+const LoadingCanvas = () => (
+  <div className="flex-1 h-[600px] flex items-center justify-center">
+    <Loader className="animate-spin" size={60} />
+  </div>
+);
+
+const Canvas = dynamic(() => import("@/components/editor/canvas"), {
+  ssr: false,
+  loading: LoadingCanvas,
+});
 
 export default function EditorPage() {
   return (
-    <main className="">
+    <main>
       <div className="flex">
         <Canvas />
         <SideMenu />
--- src/components/editor/canvas.tsx
+++ src/components/editor/canvas.tsx
@@ -2,24 +2,31 @@
 
 import { useCallback } from "react";
 import {
-  type ColorMode,
   Controls,
+  Background,
   ReactFlow,
   useEdgesState,
   useNodesState,
+  addEdge,
+  type ColorMode,
   type Edge,
   type Connection,
-  addEdge,
+  type NodeTypes,
 } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
 import { useTheme } from "next-themes";
+import { StateNode } from "./state-node";
 
-export function Canvas() {
+const nodeTypes: NodeTypes = {
+  state: StateNode,
+}
+
+export default function Canvas() {
   const { theme } = useTheme();
   const [nodes, setNodes, onNodesChange] = useNodesState(
     [
-      { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-      { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+      { id: 'Q1', type: 'state', position: { x: 0, y: 0 }, data: { label: '1' } },
+      { id: 'Q2', type: 'state', position: { x: 0, y: 100 }, data: { label: '2' } },
     ]
   );
 
@@ -30,16 +37,17 @@ export function Canvas() {
   return (
     <div className="flex-1 h-[600px]">
       <ReactFlow
-        suppressHydrationWarning
         colorMode={theme as ColorMode}
         nodes={nodes}
         edges={edges}
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        nodeTypes={nodeTypes}
         fitView
       >
         <Controls position="bottom-right" />
+        <Background />
       </ReactFlow>
     </div>
   )
