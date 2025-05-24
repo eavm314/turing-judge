2972af72090dc3418aa348fc2cfc1cf88785a1c8
Enrique Vicente <enriqueadhemar@gmail.com>
2025-05-23 21:34:56 -0400

    feat: setup playwright

---
+++ tests/home-page.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from "@playwright/test";
+
+test("Home Page", async ({ page }) => {
+  await page.goto("/");
+  await expect(
+    page.getByRole("heading", { name: "TuringJudge" }),
+  ).toBeVisible();
+  await expect(
+    page.getByRole("link", { name: "Go to Playground" }),
+  ).toBeVisible();
+  await expect(page.getByRole("button", { name: "Sign In" })).toBeVisible();
+});
