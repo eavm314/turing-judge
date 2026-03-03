6721d694f16368b974a62b0cbd9e9aee8df8729f
Enrique Vicente <enriqueadhemar@gmail.com>
2026-03-02 20:54:07 -0400

    upgrade: use next 16

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,27 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
+import nextTypescript from 'eslint-config-next/typescript';
+import prettier from 'eslint-config-prettier/flat';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.config({
-    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+const eslintConfig = defineConfig([
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  prettier,
+  {
     rules: {
       'react-hooks/exhaustive-deps': 'off',
     },
-  }),
-];
+  },
+  globalIgnores([
+    'node_modules/**',
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    'tailwind.config.ts',
+    'postcss.config.mjs',
+    'eslint.config.mjs',
+  ]),
+]);
 
 export default eslintConfig;
