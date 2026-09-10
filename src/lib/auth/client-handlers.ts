a7c05aea1ee46b4f9bab4dff53b4be9afb15712e
Enrique Vicente <enriqueadhemar@gmail.com>
2026-09-09 21:27:35 -0400

    feat: open oauth url in a new tab instead of a popup

--- src/lib/auth/client-handlers.ts
+++ src/lib/auth/client-handlers.ts
@@ -1,23 +1,33 @@
 import { revalidateAll, signIn, signOut } from '@/actions/auth';
 
 export const handleSignIn = (provider?: string) => {
+  const tab = window.open('about:blank', '_blank');
+
   return new Promise(async (resolve, reject) => {
     try {
       const url = await signIn(provider || 'google', {
         redirect: false,
         redirectTo: '/signin',
       });
-      const popup = window.open(url as string, 'Sign In', 'width=480,height=600,top=100,left=460');
-      popup?.focus();
-      const checkPopupClosed = setInterval(() => {
-        if (!popup || popup.closed) {
-          clearInterval(checkPopupClosed);
+
+      if (!tab) {
+        window.location.href = url as string;
+        return;
+      }
+
+      tab.location.href = url as string;
+      tab.focus();
+
+      const checkTabClosed = setInterval(() => {
+        if (tab.closed) {
+          clearInterval(checkTabClosed);
           revalidateAll();
           resolve(0);
         }
       }, 500);
     } catch (error) {
-      console.error('Error opening sign-in popup:', error);
+      console.error('Error opening sign-in tab:', error);
+      tab?.close();
       reject(error);
     }
   });
